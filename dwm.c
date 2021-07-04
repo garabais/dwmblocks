167d514c18c53c64e1a52ad8d6509ccb8091ca65
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2021-07-03 20:27:31 -0500

    Revert "Apply the send monitor keep focus patch"
    This reverts commit d6daec57c9ec9fb1d7cb297f4a3bcdcd616b66fb.

--- dwm.c
+++ dwm.c
@@ -1488,21 +1488,15 @@ sendmon(Client *c, Monitor *m)
 {
 	if (c->mon == m)
 		return;
-	int hadfocus = (c == selmon->sel);
 	unfocus(c, 1);
 	detach(c);
 	detachstack(c);
-	arrange(c->mon);
 	c->mon = m;
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
 	attach(c);
 	attachstack(c);
-	arrange(m);
-	if (hadfocus) {
-		focus(c);
-		restack(m);
-	} else
-		focus(NULL);
+	focus(NULL);
+	arrange(NULL);
 }
 
 void
