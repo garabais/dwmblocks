57c0be3c0a1efd828fba40ee24a61b8b3cc36051
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2021-07-04 20:27:51 -0500

    Apply status height patch

--- config.h
+++ config.h
@@ -7,6 +7,7 @@ static const unsigned int gappx     = 10;       /* gap pixel between windows */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Noto Sans:size=13", "monospace:size=10" };
 static const char col_gray1[]       = "#2E3440";
 static const char col_gray2[]       = "#4C566A";
